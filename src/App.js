c0c2cd72862e83c01a0bdb5a01e33a9335015724
IsmailAvayil <ismailavayil@gmail.com>
2023-09-13 00:26:10 +0530

    red commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import PostWithComments from "./components/PostWithComments";
 function App() {
   return (
-    <div className="App" style={{backgroundColor:"yellow",height:"100vh"}}>
+    <div className="App" style={{backgroundColor:"red",height:"100vh"}}>
       <PostWithComments/>
     </div>
   );
